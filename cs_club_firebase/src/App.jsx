428a51b93f0b36d9e8f180cccfdf3eead9c43627
Dhruv <dhruvkkothari@gmail.com>
2024-12-08 09:28:04 -0800

    week 3 code

--- cs_club_firebase/src/App.jsx
+++ cs_club_firebase/src/App.jsx
@@ -1,179 +1,90 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { auth } from './firebase/firebase.jsx'
+import { auth , db} from './firebase/firebase.jsx'
 import {GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-// import {}
+import { collection, doc, getDocs, query, setDoc, where, getDoc, onSnapshot } from "firebase/firestore";
+
+
 
 function App() {
-  const [user, setUser] = useState("")
-  
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [actualEmail, setActualEmail] = useState("");
-  const [actualPassword, setActualPassword] = useState("");
-
-
+  const [name, setName] = useState("");
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       if (currentUser){
         if (currentUser.displayName){
-          setUser(currentUser.displayName);
+          setName(currentUser.displayName);
         }
         else{
-          setUser(currentUser.email);
+          setName(currentUser.email);
         }
       }
-      else setUser("");
+      else setName("");
     });
   }, []);
 
-  
+  const fetchData = async () => {
+      // const querySnapshot = await getDocs(collection(db, "users")); // Replace "users" with your collection name
+    const q = query(collection(db, "users"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const cities = [];
+      querySnapshot.forEach((doc) => {
+          console.log(doc.data().uid);
+      });
+    });
+      
+  };
 
-  const logOut = async () => {
+  const logInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      setName(user.displayName);
+      console.log(user.displayName);
+
+      const usersRef = doc(db, "users", user.uid);
+      const userSnap = await getDoc(usersRef);
+
+      if (!userSnap.exists()) {
+        console.log("No such document!");
+        //we now want to add a new entry to our database...
+        await setDoc(usersRef, {
+          email: user.email,
+          name: user.displayName,
+          uid : user.uid
+        })
+        
+        console.log("Added new entry for user " + user.displayName);
+      } else {
+        console.log("Document already exists: ", userSnap.data());
+      }
+    } catch (error) {
+      console.error("Error during sign-in:", error.message);
+    }
+  };
+
+  const signOutOfAccount = async () => {
     signOut(auth).then(() => {
-      console.log("Sign out successful!");
-      setUser("");
+      setName("");
+      console.log("Signed out successfully");
     }).catch((error) => {
       // An error happened.
-      console.log("Error happened: " + error)
     });
-  }  
-
-  const googleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    // console.log(auth.currentUser);
-    signInWithPopup(auth, provider).then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        if (credential != null) {
-          const token = credential.accessToken;
-          const USER = result.user.displayName;
-          // console.log(USER);
-          setUser(USER);
-          console.log(result.user.uid + " " + USER);
-        }
-        // The signed-in user info.
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        console.log("There is an error!");
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  };
-  const register = async () => {
-    createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    console.log("Signed in");
-    // ...
-  })
-  .catch((error) => {
-    console.log("Error: " + error.message);
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-
-  };
-
-  
-  const emailSignIn = async () => {
-    signInWithEmailAndPassword(auth, actualEmail, actualPassword)
-  .then((userCredential) => {
-    // Signed in 
-    // const user = userCredential.user;
-    // setUser(userCredential);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-  };
+    
+  }
   return (
     <>
-      <div>
-        <Navbar bg="light" variant="light" sticky="top">
-          <Container>
-            <Navbar.Brand href="#home">Dhruv's Website :{")"}</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                Signed in as: <a href="#login">{user || "Guest"}</a>
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-
-        <div className="content">
-          <h1>Sign in with Google</h1>
-          <button onClick={googleSignIn}>
-            Log in w/ Google
-          </button>
-          {/* <p id="para1">{user !== "" ? user : "Not Logged In"}</p> */}
-        </div>
-        <h1>Or use email & password...</h1>
-        <h1>Register</h1>
-        <div>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={(event) => {
-            setEmail(event.target.value);
-          }} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label >Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={(event) => {
-            setPassword(event.target.value);
-          }} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick = {register}>
-        Register
-      </Button>
-      </div>
-      <h1>Sign In</h1>
-        <div>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={(event) => {
-            setActualEmail(event.target.value);
-          }} />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label >Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={(event) => {
-            setActualPassword(event.target.value);
-          }} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick = {emailSignIn}>
-        Sign In
-      </Button>
-      </div>
-        <button onClick = {logOut}>Sign Out</button>
-      </div>
+      <button onClick = {logInWithGoogle}>Log in with google</button>
+      <h1>{name != "" ? name : "Not signed in" }</h1>
+      <button onClick = {signOutOfAccount}>Sign out</button>
+      <button onClick = {fetchData}>Fetch Data</button>
     </>
-  );
+  )
 }
 
 export default App
--- cs_club_firebase/src/firebase/firebase.jsx
+++ cs_club_firebase/src/firebase/firebase.jsx
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth} from "firebase/auth";
+import {getFirestore} from 'firebase/firestore'
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,7 +10,15 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBwM8QD-OFjxWVJBuBw9LNCZZD_2PXlbWo",
+    authDomain: "testing-e3143.firebaseapp.com",
+    projectId: "testing-e3143",
+    storageBucket: "testing-e3143.firebasestorage.app",
+    messagingSenderId: "850744534076",
+    appId: "1:850744534076:web:40d06c5dc343bad2620f39",
+    measurementId: "G-ELBDSTEG2F"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -17,3 +26,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();  
 export const auth = getAuth(app);
+export const db = getFirestore(app);
